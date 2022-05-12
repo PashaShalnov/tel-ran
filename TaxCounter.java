7860d11d511c67ff316b3770206e493aa549eb05
Pavel Shalnov <94354424+PashaShalnov@users.noreply.github.com>
2022-05-12 11:18:21 +0300

    Update TaxCounter.java

--- TaxCounter.java
+++ TaxCounter.java
@@ -2,18 +2,20 @@
 public class TaxCounter {
 
 	public static void main(String[] args) {
-		double income = 1000;
+		double income = 5000;
 		counter(income);
 	}
 
 	private static double counter(double inc) {
+		double tax = 0;
 		if (inc > 1000) {
-			inc = inc - ((inc - 1000) / 100 * 17);
-			System.out.println(inc);
-			return inc;
+			tax = ((inc - 1000) / 100 * 17);
+			System.out.println(tax);
+			return tax;
 		} else {
-			System.out.println(inc);
-			return inc;
+			tax = 0;
+			System.out.println(tax);
+			return tax;
 		}
 	}
 
