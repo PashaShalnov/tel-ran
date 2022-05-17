120fd35891e655e02228cf32ad2c0a4a6088a473
Pavel Shalnov <94354424+PashaShalnov@users.noreply.github.com>
2022-05-17 19:03:53 +0300

    Update HWNestedLoopsAppl.java

--- HWNestedLoopsAppl.java
+++ HWNestedLoopsAppl.java
@@ -11,28 +11,28 @@ public class HWNestedLoopsAppl {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (j <= i) {
-				System.out.print("*");
+					System.out.print("*");
 				} else {
 					System.out.print("");
 				}
-				
-				}
+
+			}
 			System.out.println();
 		}
-}
-	
+	}
+
 	public static void figure2(int n) {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (j >= i && j < n - i) {
-				System.out.print("*");
+					System.out.print("*");
 				} else {
 					System.out.print(" ");
 				}
-				
-				}
+
+			}
 			System.out.println();
 		}
-}
-	
+	}
+
 }
